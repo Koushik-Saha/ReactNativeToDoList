776b581c906e58953c4ca16527348a03067a3b16
Koushik Saha <koushik.saha666@gmail.com>
2022-03-05 10:53:00 +0600

    update icons and add sign in and sign up screens

--- context/auth-context.js
+++ context/auth-context.js
@@ -128,6 +128,7 @@ const AuthProvider = (props) => {
             value={{
                 loginState,
                 theme,
+                // signIn
                 // signOut,
                 // toggleTheme
             }}
