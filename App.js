e6c6601b5b02ff1014c736b639fc126988d8e468
Koushik Saha <koushik.saha666@gmail.com>
2022-03-06 01:48:24 +0600

    add photo gallery to homepage

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {AuthProvider} from "./context/auth-context";
 import Routes from "./Routes";
+import { NativeBaseProvider } from "native-base";
 
 const App = () => {
     return (
+        <NativeBaseProvider>
         <AuthProvider>
             <Routes />
         </AuthProvider>
+        </NativeBaseProvider>
     );
 }
 
